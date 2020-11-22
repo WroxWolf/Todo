a017e3704c4cb6cc0ef56012598bb03bf40304c8
WroxWolf <wroxwolf@gmail.com>
2020-11-22 17:59:40 +0100

    index,data,lista készitése

--- my.js
+++ my.js
@@ -1,18 +1,56 @@
-let tableBox = document.getElementById("tbody");
-let todoData = []
+  
+// Változók
 
+//Elemek
+let checkboxCheckAll = document.getElementById("inputCheckAll");
+let buttonDeleteChecked = document.getElementById("buttonDeleteChecked");
+let inputAdd = document.getElementById("inputAdd");
+let buttonAdd = document.getElementById("buttonAdd");
+let divListBox = document.getElementById("listBox");
 
-function Beolvas(){
+//Feliratkozás
+$(document).ready(function(){
+    ReadIn();
+});
+
+checkboxCheckAll.addEventListener("change", OnChange_CheckAll);
+
+buttonDeleteChecked.addEventListener("click", OnClick_DeleteChecked);
+
+buttonAdd.addEventListener("click", OnCLick_AddNew);
+
+//Eseménykezelők
+function ReadIn() {
     $.ajax({
         url: "todo.json",
         method: "get",
-        dataType: "json",
+        datatype: "JSON",
         success: function(data){
-            todoData = data;
-            console.log(todoData);
-            
+            new List(divListBox, data);
         },
-        error:function(error){   
+        error: function(error){
+            console.log(error);
         }
     });
+}
+
+function OnChange_CheckAll(){
+    let event = new CustomEvent("checkAll", {detail: this.checked});
+
+    window.dispatchEvent(event);
+}
+
+function OnClick_DeleteChecked() {
+    let event = new Event("deleteChecked");
+
+    window.dispatchEvent(event);
+}
+
+function OnCLick_AddNew() {
+    let newTask = inputAdd.value;
+    inputAdd.value = "";
+
+    let event = new CustomEvent("addNew", {detail: newTask});
+
+    window.dispatchEvent(event);
 }
